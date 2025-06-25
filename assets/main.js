40af1ee4d70c7886425de69b7400704eb2019ae2
thang.thieuquang <thang.thieuquang@ncc.asia>
2025-06-25 22:32:21 +0700

    task - implement Mobile Menu

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,25 @@
 const header = document.querySelector('#header');
+const menuBar = document.querySelector('.menu__bar');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 window.addEventListener('scroll', function () {
     if (window.scrollY > 50) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
+    }
+});
+
+menuBar.addEventListener('click', function () {
+    if (window.innerWidth < 768) {
+        mobileMenu.classList.contains('active')
+            ? mobileMenu.classList.remove('active')
+            : mobileMenu.classList.add('active');
+    }
+});
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 768) {
+        mobileMenu.classList.remove('active');
     }
 });
