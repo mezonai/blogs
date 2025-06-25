603c1e13dc15710011a29ee2cfe4515a99c93e6c
khoi.huynhgia <khoi.huynhgia@ncc.asia>
2025-06-25 21:01:37 +0700

    task - implement category and list item UI

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,22 @@
+const categoryDropdown = document.getElementById("categoryDropdown");
+const overlayModal = document.getElementById("overlayModel");
+const closeBtn = document.getElementById("closeBtn");
+
+categoryDropdown.addEventListener("click", () => {
+    overlayModal.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    overlayModal.classList.remove("active");
+});
+
+// Optional: click outside to close
+overlayModal.addEventListener("click", (e) => {
+    if (e.target === overlayModal) {
+        overlayModal.classList.remove("active");
+    }
+});
+
 const header = document.querySelector('#header');
 
 window.addEventListener('scroll', function () {
