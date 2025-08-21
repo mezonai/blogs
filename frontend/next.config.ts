18dedae2bd8250ab2cb13c4d09a695dd05c459c8
tien.nguyenvan <tien.nguyenvan@ncc.asia>
2025-08-21 10:48:26 +0700

    (frontend): add remote pattern for minio storage url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '1337',
         pathname: '/uploads/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '9000',
+        pathname: '/mezonblogs/**',
+      },
     ],
   },
 };
