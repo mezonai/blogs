925535f60bd6c5ec0360af4d1f28bd5019f6b8e3
tien.nguyenvan <tien.nguyenvan@ncc.asia>
2025-08-21 10:46:56 +0700

    (backend) plugin: MinIO provider for upload plugin

--- backend/apps/blog/config/middlewares.ts
+++ backend/apps/blog/config/middlewares.ts
@@ -1,7 +1,31 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    // https://github.com/flyce/strapi-provider-upload-minio-ce?tab=readme-ov-file#pictures-cannot-be-displayed
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "localhost:9000",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "localhost:9000",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
