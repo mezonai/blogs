d4e11803da4e3f5f2ca6be36f7e7b3ba1b3d26be
tien.nguyenvan <tien.nguyenvan@ncc.asia>
2025-08-23 13:00:53 +0700

    Revert "(backend) plugin: MinIO provider for upload plugin"
    This reverts commit 925535f60bd6c5ec0360af4d1f28bd5019f6b8e3.

--- backend/apps/blog/config/plugins.ts
+++ backend/apps/blog/config/plugins.ts
@@ -24,25 +24,4 @@ module.exports = ({ env }) => ({
       maxLimit: 20,
     },
   },
-  upload: {
-    config: {
-      /**
-       * TODO:
-       * - duplicate images stored in bucket (research about thumbnails_prefix)
-       * - research `providerOptions.expiry`
-       * - try create your own MinIO provider
-       */
-      provider: 'strapi-provider-upload-minio-ce',
-      providerOptions: {
-        accessKey: env('MINIO_ACCESS_KEY', 'minioadmin'),
-        secretKey: env('MINIO_SECRET_KEY', 'minioadmin'),
-        bucket: env('MINIO_BUCKET', 'mezonblogs'),
-        endPoint: env('MINIO_ENDPOINT', 'localhost'),
-        port: env('MINIO_PORT', 9000),
-        useSSL: env('MINIO_USE_SSL', false), // default is false for localhost
-        folder: env('MINIO_FOLDER', 'images'),
-        private: env('MINIO_PRIVATE', false),
-      },
-    },
-  },
 });
