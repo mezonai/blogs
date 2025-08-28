dd627b34d09874fe6c6f742b7af3842f0462ea90
Duy Nguyen Xuan <duy.nguyenxuan@ncc.asia>
2025-08-28 18:07:53 +0700

    fix: correct protocol in getHostPart function to use HTTPS

--- backend/packages/providers/strapi-provider-upload-minio-ce/index.js
+++ backend/packages/providers/strapi-provider-upload-minio-ce/index.js
@@ -22,7 +22,7 @@ module.exports = {
     const getHostPart = () => {
       const protocol = isUseSSL ? 'https://' : 'http://';
       const portSuffix = ((isUseSSL && +port === 443) || (isUseSSL && +port === 80)) ? '' : `:${port}`;
-      return 'http://' + endPoint + '/';
+      return 'https://' + endPoint + '/';
     };
     const getFilePath = (file) => {
       const hostPart = getHostPart() + '/';
