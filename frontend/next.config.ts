4d0a45f7cfa70771f9bd5bd84ddcc767cf2e49d9
tien.nguyenvan <tien.nguyenvan@ncc.asia>
2025-09-06 16:25:54 +0700

    fix: update README for nginx deployment and adjust Next.js config to prevent redirect loops

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ...(process.env.ENV === 'production'
-    ? { basePath: '/blogs' }
-    : {}),
+  basePath: '/blogs',
+  /**
+   * By default (false), Next.js redirects /path to /path/ (or vice versa, depending on the trailingSlash config in next.config.js). 
+   * With basePath: '/blogs', it might redirect /blogs to /blogs/ repeatedly if the server doesn’t recognize the path correctly.
+   * In this case, nginx server config is:
+   * location /blogs/ {
+      proxy_pass http://127.0.0.1:1338;
+    }
+   * so make trailing slash true to resolve the redirect loop and CSS loading issues!   
+   */
+  trailingSlash: true,
   images: {
     remotePatterns: [
       {
